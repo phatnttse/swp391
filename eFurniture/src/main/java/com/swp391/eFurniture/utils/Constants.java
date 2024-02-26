d54e32fc3b96cf0462d16d7bd5a9dab7982fe986
phatnttse <127718651+phatnttse@users.noreply.github.com>
2024-02-26 21:59:41 +0700

    create folder FE

--- eFurniture/src/main/java/com/swp391/eFurniture/utils/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.swp391.eFurniture.utils;
-
-public class Constants {
-    public final static class EMAIL_FEATURE {
-        public final static String EMAIL_FILE = "views/email";
-    }
-}

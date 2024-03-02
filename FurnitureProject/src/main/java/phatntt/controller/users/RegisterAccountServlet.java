8a1fa48753931c235e9f990064828f7cca676151
phatnttse <127718651+phatnttse@users.noreply.github.com>
2024-03-02 16:35:27 +0700

    update validate feature

--- FurnitureProject/src/main/java/phatntt/controller/users/RegisterAccountServlet.java
+++ FurnitureProject/src/main/java/phatntt/controller/users/RegisterAccountServlet.java
@@ -15,7 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import phatntt.dto.UsersRegisterError;
+import phatntt.dto.ErrorDTO;
 import phatntt.util.Key_Utils;
 import phatntt.util.Constants;
 
@@ -37,66 +37,8 @@ public class RegisterAccountServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        request.setCharacterEncoding("UTF-8");
-        
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-        String confirm = request.getParameter("confirm_password");
-        String given_name = request.getParameter("given_name");
-        String family_name = request.getParameter("family_name");
-        
-             
-        UsersRegisterError errors = new UsersRegisterError();
-        boolean foundErr = false;
-        
-        ServletContext context = this.getServletContext();
-        Properties siteMaps = (Properties)context.getAttribute("SITEMAPS");
-        
-        
-        String url = siteMaps.getProperty(Constants.SignUpFeatures.REGISTER_PAGE);
-        try {
-          
-                    
 
-//            if (!confirm.trim().equals(password.trim())) {
-//                foundErr = true;
-//                errors.setConfirmNotMatch(
-//                        siteMaps.getProperty(Constants.SignUpFeatures.CONFIRM_NOTMATCHED_ERR_MESSAGE));
-//            }
-//            if (given_name.trim().length() < 2 || given_name.trim().length() > 50) {
-//                foundErr = true;
-//                errors.setFullnameLengthError(
-//                        siteMaps.getProperty(Constants.SignUpFeatures.FULLNAME_LENGTH_ERR_MESSAGE));
-//            }        
-//            if (foundErr) { // error occur
-//                // cache to attribute then 
-//                request.setAttribute("REGISTER_ERRORS", errors);
-//               
-//            } else { //no error
-                Key_Utils utils = Key_Utils.getInstance();
-                String newPassword = utils.hashPassword(password);
-                HttpSession session = request.getSession();
-                session.setAttribute("EMAIL", email);
-                session.setAttribute("GIVEN_NAME",  given_name );
-                session.setAttribute("FAMILY_NAME", family_name);
-                session.setAttribute("PASSWORD", newPassword);              
-                
-                url = siteMaps.getProperty(Constants.VerifyEmailFeatures.EMAIL_PAGE);
-                
-            
-//        } catch (SQLException ex) {
-//            log("RegisterAccountServlet_SQLException: " + ex.getMessage());                                 
-//        } catch (NamingException ex) {
-//            log("RegisterAccountServlet_NamingException: " + ex.getMessage());
-        } 
-
-        finally {
-            RequestDispatcher rd = request.getRequestDispatcher(url);
-            rd.forward(request, response);
-        }
     }
-    
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -124,7 +66,60 @@ public class RegisterAccountServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String confirm = request.getParameter("confirm_password");
+        String given_name = request.getParameter("given_name");
+        String family_name = request.getParameter("family_name");
+
+        ErrorDTO errors = new ErrorDTO();
+        boolean foundErr = false;
+
+        ServletContext context = this.getServletContext();
+        Properties siteMaps = (Properties) context.getAttribute("SITEMAPS");
+
+        String url = siteMaps.getProperty(Constants.SignUpFeatures.REGISTER_PAGE);
+        if (!password.trim().matches(siteMaps.getProperty(Constants.ValidateFeatures.PASSWORD_REGEX))) {
+            foundErr = true;
+            errors.setConfirmNotMatch(
+                    siteMaps.getProperty(Constants.ValidateFeatures.PASSWORD_REGEX_ERR_MSG));
+        }
+
+        if (!confirm.trim().equals(password.trim())) {
+            foundErr = true;
+            errors.setConfirmNotMatch(
+                    siteMaps.getProperty(Constants.ValidateFeatures.CONFIRM_NOTMATCH_ERR_MSG));
+        }
+        if (given_name.trim().length() < 2 || given_name.trim().length() > 50) {
+            foundErr = true;
+            errors.setGivenNameLengthError(
+                    siteMaps.getProperty(Constants.ValidateFeatures.GIVENNAME_LENGTH_ERR_MSG));
+        }
+        if (family_name.trim().length() < 2 || given_name.trim().length() > 50) {
+            foundErr = true;
+            errors.setGivenNameLengthError(
+                    siteMaps.getProperty(Constants.ValidateFeatures.GIVENNAME_LENGTH_ERR_MSG));
+        }
+        if (foundErr) { // error occur 
+
+            request.setAttribute("REGISTER_ERRORS", errors);
+
+        } else { //no error
+            Key_Utils utils = Key_Utils.getInstance();
+            String newPassword = utils.hashPassword(password);
+            HttpSession session = request.getSession();
+            session.setAttribute("EMAIL", email);
+            session.setAttribute("GIVEN_NAME", given_name);
+            session.setAttribute("FAMILY_NAME", family_name);
+            session.setAttribute("PASSWORD", newPassword);
+
+            url = siteMaps.getProperty(Constants.VerifyEmailFeatures.EMAIL_PAGE);
+        }
+        RequestDispatcher rd = request.getRequestDispatcher(url);
+        rd.forward(request, response);
     }
 
     /**
